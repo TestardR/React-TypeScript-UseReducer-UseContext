e820b7f4ff7e52ce651e43d61608828d72796748
rtestard <rtestard@opensense.fr>
2019-11-20 16:10:17 +0100

    adding interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React, { Fragment, useContext, useReducer } from "react";
 import { Store } from "./Store";
 
+interface Iaction {
+  type: string
+  payload: number
+}
+
 const App: React.FC = () => {
-  const reducer = (state: number, action: string): number => {
-    switch (action) {
+  const reducer = (state: number, action: Iaction): number => {
+    switch (action.type) {
       case "ADD":
         return state + 1;
       case "SUB":
@@ -26,9 +31,9 @@ const App: React.FC = () => {
       <h1>Rick and Morty</h1>
       <p>Pick your favourite episode !</p>
       <div>{count}</div>
-      <button onClick={() => dispatch("ADD")}>ADD</button>
-      <button onClick={() => dispatch("SUB")}>SUB</button>
-      <button onClick={() => dispatch("RES")}>RES</button>
+      <button onClick={() => dispatch({type: 'ADD', payload: 0})}>ADD</button>
+      <button onClick={() => dispatch({type: 'SUB', payload: 0})}>SUB</button>
+      <button onClick={() => dispatch({type: 'RES', payload: 0})}>RES</button>
     </Fragment>
   );
 };
